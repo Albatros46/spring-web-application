63b3dc058554a5bc4ab41d05205a3fc907f4c4d8
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2021-06-08 21:29:26 +0200

    CourseraSpring Boot Course

---
+++ src/test/java/com/servet/CourseraSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseraSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
